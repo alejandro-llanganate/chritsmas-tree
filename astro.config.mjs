ead7a6a3447a2ed63657b73957f8776399f4a601
Alejandro Llanganate <alejandro.llanganate@owasp.org>
2025-01-19 19:02:02 -0500

    fix: type

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://alejandro-llanganate.github.io',
   base: '/chritsmas-tree',
+  outDir: './dist',
   output: 'static',
   build: {
     assets: 'assets'
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,5 +1,5 @@
 export const ROUTES = {
-    HOME: '/chritsmas-tree/',
-    INTRO: '/chritsmas-tree/introduccion',
-    TABLERO: '/chritsmas-tree/tablero-pistas'
+    HOME: '/',
+    INTRO: '/introduccion',
+    TABLERO: '/tablero-pistas'
 }; 
